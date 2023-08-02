f1b450d466ea7f0ae83fdf15a9b7bf0ffa7059de
Kacper Klassa <kacperklassa@gmail.com>
2023-08-02 15:43:51 +0200

    add base type annotations to crud functions

--- src/app/crud.py
+++ src/app/crud.py
@@ -1,32 +1,33 @@
 from sqlmodel import Session, SQLModel, select
+from typing import Optional, List
 
 
-def get_one(session: Session, model: SQLModel, id: int):
+def get_one(session: Session, model: SQLModel, id: int) -> Optional[SQLModel]:
     statement = select(model).where(model.id == id)
     result = session.exec(statement)
     return result.one()
 
 
-def get_all(session: Session, model: SQLModel):
+def get_all(session: Session, model: SQLModel) -> List[SQLModel]:
     statement = select(model)
     result = session.exec(statement)
     return result.all()
 
 
-def create_one(session: Session, model: SQLModel):
+def create_one(session: Session, model: SQLModel) -> SQLModel:
     session.add(model)
     session.commit()
     session.refresh(model)
     return model
 
 
-def update_one(session: Session, model: SQLModel):
+def update_one(session: Session, model: SQLModel) -> Optional[SQLModel]:
     session.merge(model)
     session.commit()
     return model
 
 
-def delete_one(session: Session, model: SQLModel):
+def delete_one(session: Session, model: SQLModel) -> Optional[SQLModel]:
     session.delete(model)
     session.commit()
     return model
