a67f0388f39ec27c63e842ab2732e93cec8b3441
Kacper Klassa <kacperklassa@gmail.com>
2023-08-02 14:20:33 +0200

    refactor main.py

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ from alembic.config import Config
 from fastapi import Depends, FastAPI
 from fastapi.responses import RedirectResponse
 from sqlmodel import Session
+from typing import Annotated, List, Optional
 
 from app.database import engine
 import app.crud as crud
@@ -20,6 +21,9 @@ def get_db():
         db.close()
 
 
+DBSession = Annotated[Session, Depends(get_db)]
+
+
 @app.on_event("startup")
 def on_startup():
     alembic_cfg = Config("alembic.ini")
@@ -31,14 +35,14 @@ async def root():
     return RedirectResponse(url="/docs")
 
 
-@app.get("/characters/{id}")
-@app.get("/characters/")
-async def get_characters(id: int = None, db: Session = Depends(get_db)):
+@app.get("/characters/{id}", response_model=Optional[models.Character])
+@app.get("/characters/", response_model=List[models.Character])
+async def get_characters(db: DBSession, id: int = None):
     if id:
         return crud.get_one(db, models.Character, id)
     return crud.get_all(db, models.Character)
 
 
-@app.post("/characters/")
-async def create_character(character: models.Character, db: Session = Depends(get_db)):
+@app.post("/characters/", response_model=models.Character)
+async def create_character(db: DBSession, character: models.Character):
     return crud.create_one(db, character)
